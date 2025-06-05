7268615daa2d4fee8aa91bb4bb4cf01773ea1897
L-FernandesAI <loanlopes91@gmail.com>
2025-06-05 09:08:39 +0100

    Corrige URL do backend no .env e prepara pro redeploy

--- Migrations/20250605080801_AddPendingChanges.Designer.cs
+++ Migrations/20250605080801_AddPendingChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EstoqueApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EstoqueApp.Migrations
 {
     [DbContext(typeof(EstoqueDbContext))]
-    partial class EstoqueDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250605080801_AddPendingChanges")]
+    partial class AddPendingChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.5");
